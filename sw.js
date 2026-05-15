060cb2270b1c0d3d198fffdcab3e08786a359c69
altmann02 <77552143+altmann02@users.noreply.github.com>
2026-05-15 08:32:43 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,37 @@
+const CACHE_NAME = 'helena-v1';
+const ASSETS = [
+  '/helena-marketing/',
+  '/helena-marketing/index.html',
+  'https://fonts.googleapis.com/css2?family=DM+Sans:wght@300;400;500&display=swap'
+];
+
+// Instala e faz cache dos arquivos principais
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+// Ativa e limpa caches antigos
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+// Serve do cache quando offline, rede quando online
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    fetch(e.request)
+      .then(res => {
+        const clone = res.clone();
+        caches.open(CACHE_NAME).then(cache => cache.put(e.request, clone));
+        return res;
+      })
+      .catch(() => caches.match(e.request))
+  );
+});
